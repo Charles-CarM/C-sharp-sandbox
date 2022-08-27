42dcbbad519a386099292f932a821e0d91b32912
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-27 10:15:06 +0800

    data types

--- Program.cs
+++ Program.cs
@@ -5,18 +5,18 @@ namespace System
     {
         static void Main(string[] args)
 
-            //story with variable replacements
+        //data types
 
         {
+            string phrase = "Breakfast club";
+            char grade = 'T';
 
-            string characterName = "Henry";
-            int characterAge;
-            characterAge = 35;
+            int age = -23;
+            double gpa = 3.35;
 
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("Hes was " + characterAge + " years old");
-            Console.WriteLine("He really liked the name " + characterName);
-            Console.WriteLine("But didn't like being " + characterAge);
+            bool isBeautiful = true;
+
+            Console.WriteLine(isBeautiful);
 
             Console.ReadLine();
 
