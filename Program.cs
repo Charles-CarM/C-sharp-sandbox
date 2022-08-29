71b51356e67f86cccebf04c479e43f7bcfdda171
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-29 14:43:52 +0800

    while...do while

--- Program.cs
+++ Program.cs
@@ -5,46 +5,20 @@ namespace System
     {
         static void Main(string[] args)
 
-        //switch statement
+        //While loops
 
         {
-            Console.WriteLine(GetDay(45));
+            int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+
+            } while (index <= 5) ;
+        
             Console.ReadLine();
         }
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch (dayNum)
-            {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid";
-                    break;
-            }
-            return dayName;
-        }
-
+     
     }
 }
 
