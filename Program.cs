38122f3643df2d256475d69ee6d83e626b01f4e6
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-29 16:08:33 +0800

    guessing

--- Program.cs
+++ Program.cs
@@ -5,17 +5,54 @@ namespace System
     {
         static void Main(string[] args)
 
-        //While loops
+        //Guessing game
 
         {
-            int index = 6;
+            //string secretWord = "hamster";
+            //string guess = "";
+            //int guessCount = 0;
+
+            //while (guess != secretWord)
+            //{
+            //    Console.Write("guess a word: ");
+            //    guess = Console.ReadLine();
+
+            //    //Console.WriteLine("guess again");
+            //    guessCount++;
+            //}
+            //Console.Write("You win, number of guesses: " + guessCount);
+
+            string passWord = "orangutan";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
             do
             {
-                Console.WriteLine(index);
-                index++;
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("enter a guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
 
-            } while (index <= 5) ;
-        
+            } while (guess != passWord && !outOfGuesses);
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("you lose");
+            }
+            else
+            {
+                Console.WriteLine("you win!");
+
+            }
+
             Console.ReadLine();
         }
      
