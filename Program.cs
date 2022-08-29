f080e158d9da7d35b07b6d1311443e43ce8e9bfc
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-29 13:08:37 +0800

    better calc

--- Program.cs
+++ Program.cs
@@ -5,33 +5,39 @@ namespace System
     {
         static void Main(string[] args)
 
-        //If statements
+        //Next level calculator
 
         {
+            Console.Write("enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(GetMax(15, 10, 13));
+            Console.Write("Enter operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+
+            }else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+
+            }else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }else
+            {
+                Console.WriteLine("you messed up somewhere");
+            }
 
             Console.ReadLine();
-        }
-
-        static int GetMax(int num1, int num2, int num3)
-        {
-            int result;
-
-            if (num1 >= num2 && num1 >= num3)
-            {
-                result = num1;
-            }
-            else if(num2 >= num1 && num2 >= num3)
-            {
-                result = num2;
-            }
-            else
-            {
-                result = num3;
-            }
-
-            return result;
         }
 
     }
