5dd1240a151c81703b82d7637050ac531bf24e4b
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-29 12:48:53 +0800

    If

--- Program.cs
+++ Program.cs
@@ -5,74 +5,33 @@ namespace System
     {
         static void Main(string[] args)
 
-        //If Else statements
+        //If statements
 
         {
-            bool isHungry = true;
-            bool isDead = true;
-            bool isCloudy = false;
 
+            Console.WriteLine(GetMax(15, 10, 13));
 
-            Console.WriteLine("I wake up");
             Console.ReadLine();
-            if (isHungry)
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+
+            if (num1 >= num2 && num1 >= num3)
             {
-                Console.WriteLine("eat breakfast");
+                result = num1;
+            }
+            else if(num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
             }
             else
             {
-                Console.WriteLine("I'll skip breakfast today");
+                result = num3;
             }
-            Console.ReadLine();
 
-            Console.WriteLine("I look at my phone");
-            Console.ReadLine();
-            if (isDead)
-            {
-                Console.WriteLine("I charge my phone");
-            }
-            else
-            {
-                Console.WriteLine("phone has full juice");
-            }
-            Console.ReadLine();
-
-            Console.WriteLine("I leave my house");
-            Console.ReadLine();
-            if (isCloudy)
-            {
-                Console.WriteLine("I bring an umbrella");
-            }
-            else
-            {
-                Console.WriteLine("I bring sunglasses");
-            }
-            Console.ReadLine();
-               
-
-            //bool isMale = false;
-            //bool isTall = false;
-
-
-            //if (isMale && isTall)
-            //{
-            //    Console.WriteLine("You are a tall male");
-            //}else if(isMale && !isTall)
-            //{
-            //    Console.WriteLine("you are a short male");
-
-            //}else if( !isMale  && isTall)
-            //{
-            //    Console.WriteLine("you are not a male, but you are tall");
-
-            //}
-            //else
-            //{
-            //    Console.WriteLine("you are not male and not tall");
-            //}
-
-            //Console.ReadLine();
-
+            return result;
         }
 
     }
