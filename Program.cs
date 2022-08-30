1a23957f7e45aa3fb05e03a4783057104e784a13
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-31 07:54:43 +0800

    object methods

--- Program.cs
+++ Program.cs
@@ -5,23 +5,22 @@ namespace Sandbox
     {
         static void Main(string[] args)
 
-        //Constructors
+        //Object Methods
 
         {
-            Book book1 = new Book("Millennial Money", "Patrick O'Shaugnessy", 250);            
-            Book book2 = new Book("Flash Boys", "Michael Lewis", 290);
-            Book book3 = new Book("The Great Gatsby", "F.Scott Fitzgerald", 237);
-            Book book4 = new Book();
+            Student student1 = new Student("Kim", "Communications", 3.1);            
+            Student student2 = new Student("Hannah", "Petroleum Engineering", 3.7);
+            Student student3 = new Student("Alex", "Finance", 2.9);
+            //Student student4 = new Student();
 
-            book4.author = "Aldous Huxley";
-            book4.title = "The Island";
-            book4.pages = 330;
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+            Console.WriteLine(student3.HasHonors()); 
 
-            Console.WriteLine($"{book1.title} by {book1.author}, {book1.pages} pages.\n");
-            Console.WriteLine($"{book2.title} by {book2.author}, {book2.pages} pages.\n");
-            Console.WriteLine($"{book3.title} by {book3.author}, {book3.pages} pages.\n");
+
             Console.ReadLine();
         }
+       
 
     }
 }
