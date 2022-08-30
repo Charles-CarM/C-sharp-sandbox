53703cdb9fab719c48213edf4d8eb4fa85279d73
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-30 17:39:25 +0800

    classes and objects

--- Program.cs
+++ Program.cs
@@ -1,30 +1,29 @@
 ﻿
-namespace System
+namespace Sandbox
 {
     class Program
     {
         static void Main(string[] args)
 
-        //Exception handling
+        //Classes & Objects
 
         {
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+            Book book1 = new Book();
+                book1.title = "Millennial Money";
+                book1.author = "Patrick O'Shaugnessy";
+                book1.pages = 250;
 
-                Console.WriteLine(num1 / num2);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Book book2 = new Book();
+                book2.title = "Flash Boys";
+                book2.author = "Michael Lewis";
+                book2.pages = 290;
+
+            Book book3 = new Book();
+                book3.title = "The Great Gatsby";
+                book3.author = "F.Scott Fitzgerald";
+                book3.pages = 237;
+
+            Console.WriteLine(book2.pages);
 
             Console.ReadLine();
         }
