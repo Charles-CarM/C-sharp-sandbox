5b0d70604fb7e85ca36e04be8157453a765d11aa
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-08-31 09:24:50 +0800

    static class methods

--- Program.cs
+++ Program.cs
@@ -5,17 +5,10 @@ namespace Sandbox
     {
         static void Main(string[] args)
 
-        //static class attributes
+        //static class methods
 
         {
-            Song watchTower = new Song("All along the watch tower", "Jimmy Hendrix", 265);
-            Console.WriteLine(Song.songCount);
-
-            Song stairWay = new Song("Stairway to Heaven", "Led Zeppelin", 250);
-            //Console.WriteLine(stairWay.getSongCount());
-
-            Song one = new Song("One", "Metallica", 279);
-            //Console.WriteLine(one.getSongCount());
+            UsefulTools.SayHi("Sarah");
 
             Console.ReadLine();
         }
